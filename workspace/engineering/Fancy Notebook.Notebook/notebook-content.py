12b7266500b8f17fcce410a81136f490939059e0
mbohuschAdesso <MARCEL.BOHUSCH@ADESSO.DE>
2025-04-26 00:43:29 +0200

    Committing 1 item from workspace a1d1b144-4d84-450c-a18a-bbdd5c01366f

--- workspace/engineering/Fancy Notebook.Notebook/notebook-content.py
+++ workspace/engineering/Fancy Notebook.Notebook/notebook-content.py
@@ -6,15 +6,91 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "0d3a6f5d-650b-4299-9dc7-0dedded14dd9",
+# META       "default_lakehouse_name": "Bronze",
+# META       "default_lakehouse_workspace_id": "c981cb11-db75-4141-b3a8-e8dd2d72e73a",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "0d3a6f5d-650b-4299-9dc7-0dedded14dd9"
+# META         }
+# META       ]
+# META     }
+# META   }
 # META }
 
 # CELL ********************
 
-# Welcome to your new notebook
-# Type here in the cell editor to add code!
+api_key = '59f72143983cfee271ddddd4015eeb89'
 
-# here could be some fancy code
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+import requests
+from datetime import date
+
+# Get today's date
+today = date.today().isoformat()
+
+url = 'http://api.marketstack.com/v1/eod'
+
+params = {
+    'access_key': api_key,
+    'symbols': 'AAPL',
+    'sort': 'DESC',
+    'date_from': today,
+    'date_to': today,
+    'limit': 1
+}
+
+# Make the request
+response = requests.get(url, params=params)
+
+# Process the response
+data = response.json().get("data", []) 
+data
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType
+
+schema = StructType([
+    StructField("open", DoubleType(), True),
+    StructField("high", DoubleType(), True),
+    StructField("low", DoubleType(), True),
+    StructField("close", DoubleType(), True),
+    StructField("volume", DoubleType(), True),
+    StructField("adj_high", DoubleType(), True),
+    StructField("adj_low", DoubleType(), True),
+    StructField("adj_close", DoubleType(), True),
+    StructField("adj_open", DoubleType(), True),
+    StructField("adj_volume", DoubleType(), True),
+    StructField("split_factor", DoubleType(), True),
+    StructField("dividend", DoubleType(), True),
+    StructField("symbol", StringType(), True),
+    StructField("exchange", StringType(), True),
+    StructField("date", StringType(), True)  # Or TimestampType() if you parse it
+])
+
+# Create Spark DataFrame using schema
+df = spark.createDataFrame(data, schema=schema)
+
+# Write to Delta table
+df.write.format("delta").mode("append").saveAsTable("StockData.AAPL")
 
 
 # METADATA ********************
